1b516a91fc5c7661512dab6f489ce90023690e18
bettlechampion <bettlechampion5@gmail.com>
2024-09-13 12:34:32 +0530

    Delete staticfiles/js directory

--- staticfiles/js/index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) { // Adjust this value as needed
-      navbar.classList.add('navbar-scrolled');
-    } else {
-      navbar.classList.remove('navbar-scrolled');
-    }
-  });
